5fabd3afe534dca33a9ecf7c7e15bcbb1d71a708
Gaille Chua <gaillechua@gailles-mbp.lan>
2021-11-01 16:27:50 -0700

    Adds comments to index.js

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ var LaunchDarkly = require('launchdarkly-node-server-sdk');
 // Replace SDK-KEY with the provided SDK Key
 var ldclient = LaunchDarkly.init('SDK-KEY');
 
+
+// Context that LD pulls for targeting purposes
 var user = {
   firstName: 'Bob',
   lastName: 'Loblaw',
