85f721cc003042edad56552baf2a324ba1671ccc
Gaille Chua <gaillechua@gailles-mbp.lan>
2021-11-01 16:38:10 -0700

    Updates to index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var ldclient = LaunchDarkly.init('SDK-KEY');
 var user = {
   firstName: 'Bob',
   lastName: 'Loblaw',
-  key: 'unique_id',
+  key: 'user_1234',
   custom: {
     // groups: 'beta_testers'
   }
@@ -16,14 +16,8 @@ var user = {
 
 ldclient.once('ready', function() {
   ldclient.variation('first-feature', user, false, function(err, showFeature) {
+    // Displays a message in your Terminal if the first-feature value has been evaluated to true or false
     console.log("SDK successfully connected! The value of first-feature is " + showFeature + " for " + user.key)
-    // Close the LaunchDarkly SDK, after ensuring that analytics events have been delivered.
-
-    // IMPORTANT: in a real application, you would only call close() when the application is
-    // about to quit-- NOT after every call to variation(). The reason that this step is
-    // inside the variation handler is that we want it to happen after the SDK has been
-    // initialized and after the flag has been evaluated. Node.js will not allow the
-    // application to exit as long as the SDK is still running.
     ldclient.flush(function() {
       ldclient.close();
     });
