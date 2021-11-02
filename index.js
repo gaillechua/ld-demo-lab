b13c478d1909aed75c8940df05a825c7ca48b581
Gaille Chua <gaillechua@gailles-mbp.lan>
2021-11-01 17:02:05 -0700

    Index.js

--- index.js
+++ index.js
@@ -15,9 +15,10 @@ var user = {
 };
 
 ldclient.once('ready', function() {
+// This is calling the feature flag which will look at the flag definition you created in LD, the user object that you pass in at runtime (see user object above), and also a fallback value to serve in case the initialization isn't successful
   ldclient.variation('first-feature', user, false, function(err, showFeature) {
     // Displays a message in your Terminal if the first-feature value has been evaluated to true or false
-    console.log("SDK successfully connected! The value of first-feature is " + showFeature + " for " + user.key)
+    console.log("The value of first-feature is " + showFeature + " for " + user.key)
     ldclient.flush(function() {
       ldclient.close();
     });
